f76a86388d4394ecba6fc4e52860ded5b1325398
Dario <dario.fuzinato@meetrics.com>
2017-08-03 11:37:24 +0200

    modify middleware chain

--- index.js
+++ index.js
@@ -2,32 +2,18 @@ const express = require('express');
 var app = express();
 
 // Add headers
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// Serve static files
 app.use(express.static('public', {
   etag: false
 }));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-
-    // // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 var port = process.env.PORT || 3000;
+
 app.listen(port, function () {
   console.log('Listening to port: ' + port);
 });
