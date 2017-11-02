a8758308e5ac99edf8180adaed53fda7460cb1dc
Dario <dario.fuzinato@meetrics.com>
2017-11-02 13:02:02 +0100

    change cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 // Add headers
 app.use(function (req, res, next) {
     // res.header("Access-Control-Allow-Origin", "https://developer.jwplayer.com");
-    res.header("Access-Control-Allow-Origin", "http://s418.mxcdn.net");
+    res.header("Access-Control-Allow-Origin", "http://wigo.teads.tv");
     // res.header('Access-Control-Allow-Credentials', true);
 
     next();
